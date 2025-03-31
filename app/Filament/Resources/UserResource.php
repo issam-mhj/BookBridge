0dc49fa001d56eb46598efb00c5d992b1b08d039
issam-mhj <issammahtaj02@gmail.com>
2025-03-31 12:42:21 +0000

    admin:auth and liste users

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Users';
+    protected static ?string $slug = 'users';
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->query(User::query())
+            ->columns([
+                TextColumn::make('id')->label('ID')->sortable(),
+                TextColumn::make('name')->label('Name')->searchable(),
+                TextColumn::make('email')->label('Email')->searchable(),
+                TextColumn::make('created_at')->label('Joined')->date(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/UsersResource/Pages/ListUsers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\UsersResource\Pages;
+
+use App\Filament\Resources\UsersResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ListUsers extends ViewRecord
+{
+    protected static string $resource = UsersResource::class;
+    
+}
